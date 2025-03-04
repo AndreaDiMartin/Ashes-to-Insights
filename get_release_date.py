5da1d727bfcbef2fc25cac1ad41b7271097d5632
SandibelS <17-10614@usb.ve>
2025-03-04 16:46:43 -0500

    Nueva version

--- get_release_date.py
+++ get_release_date.py
@@ -1,9 +1,11 @@
 import time
+from pig_util import outputSchema
 
-def get(epoch):
-    new_epoch = epoch[:-3]
-    formatted_time = time.strftime('%Y %m %d', time.localtime(new_epoch))
-    print(formatted_time)
-    return formatted_time
-
-
+@outputSchema('formatted_time:chararray')
+def evaluate(epoch):
+    try:
+        new_epoch = int(epoch[:-3]) #convert to int
+        formatted_time = time.strftime('%Y-%m-%d', time.localtime(new_epoch))
+        return formatted_time
+    except (ValueError, TypeError, IndexError) as e:
+        return None # Handle errors gracefully.

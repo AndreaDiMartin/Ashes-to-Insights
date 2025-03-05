20464267d744c6cb921e9987afdb9b6f8650082a
SandibelS <17-10614@usb.ve>
2025-03-04 22:05:39 -0500

    Nuevas funciones para obtener los campos de una fecha de lanzamiento

--- get_release_date.py
+++ get_release_date.py
@@ -1,11 +1,38 @@
 import time
 from pig_util import outputSchema
 
-@outputSchema('formatted_time:chararray')
-def evaluate(epoch):
+@outputSchema('year:chararray')
+def get_year(epoch):
     try:
-        new_epoch = int(epoch[:-3]) #convert to int
-        formatted_time = time.strftime('%Y-%m-%d', time.localtime(new_epoch))
-        return formatted_time
+        new_epoch = int(epoch[:-3])
+        year = time.strftime('%Y', time.localtime(new_epoch))
+        return year
     except (ValueError, TypeError, IndexError) as e:
-        return None # Handle errors gracefully.
+        return None 
+
+@outputSchema('month:chararray')
+def get_month(epoch):
+    try:
+        new_epoch = int(epoch[:-3])
+        month = time.strftime('%m', time.localtime(new_epoch))
+        return month
+    except (ValueError, TypeError, IndexError) as e:
+        return None 
+
+@outputSchema('day:chararray')
+def get_day(epoch):
+    try:
+        new_epoch = int(epoch[:-3])
+        day = time.strftime('%d', time.localtime(new_epoch))
+        return day
+    except (ValueError, TypeError, IndexError) as e:
+        return None 
+
+@outputSchema('day_of_week:chararray')
+def get_day_of_week(epoch):
+    try:
+        new_epoch = int(epoch[:-3])
+        day_of_week = time.strftime('%a', time.localtime(new_epoch))
+        return day_of_week
+    except (ValueError, TypeError, IndexError) as e:
+        return None 

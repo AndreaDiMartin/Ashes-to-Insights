cd80a59e996e1783144544a9279f64ae131505d1
AndreaDiMartin <18-10826@usb.ve>
2025-03-13 17:49:15 -0400

    Add SortGenresByYear class with mapper and reducer for sorting genre counts by year

--- src/main/java/mapreduce/SortGenresByYear.java
+++ src/main/java/mapreduce/SortGenresByYear.java
@@ -24,43 +24,41 @@ import org.apache.hadoop.util.*;
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
-
-public class PopularGenresByYear extends Configured implements Tool {
-    private Schema genreYear = new Schema.Parser().parse("{\"type\":\"record\",\"name\":\"genreYear\",\"fields\":[{\"name\":\"key\",\"type\":\"int\"},{\"name\":\"value\",\"type\":\"string\"}]}");
-    public static class PopularGenresMapper extends AvroMapper<GenericRecord, Pair<CharSequence, Integer>> {
+public class SortGenresByYear extends Configured implements Tool {
+    private Schema genreYearCount = new Schema.Parser().parse("{\"type\":\"record\",\"name\":\"genreYear\",\"fields\":[{\"name\":\"key\",\"type\":\"string\"},{\"name\":\"value\",\"type\":\"int\"}]}");
+    public static class SortPopularGenresMapper extends AvroMapper<GenericRecord, Pair<Integer, CharSequence>> {
         @Override
-        public void map(GenericRecord record,AvroCollector<Pair<CharSequence, Integer>> collector, Reporter reporter)
+        public void map(GenericRecord record,AvroCollector<Pair<Integer, CharSequence>> collector, Reporter reporter)
                 throws IOException {
-            String year = record.get("key").toString();
-            String[] listOfGenres = record.get("value").toString().split(", ");
-            for(String genre: listOfGenres){
-                if(!genre.trim().isEmpty() && !genre.trim().equals("Unknown")){
-                    collector.collect(new Pair<CharSequence, Integer>(year + " - " + genre, 1));
-                }
-            }
-        
+            String yearGenre = record.get("key").toString();
+            String[] yearGenreSplit = yearGenre.split(" - ");
+            Integer year = Integer.parseInt(yearGenreSplit[0]);
+            CharSequence genre = yearGenreSplit[1];
+            Integer count = Integer.parseInt(record.get("value").toString());
+            Integer genreCount = year + count;
+            collector.collect(new Pair<Integer, CharSequence>(genreCount, genre));
         }
     }
 
-    public static class PopularGenresReducer extends AvroReducer<CharSequence, Integer, Pair<CharSequence, Integer>>{
+    public static class SortPopularGenresReducer extends AvroReducer<Integer, CharSequence, Pair<Integer, CharSequence>>{
         @Override
-        public void reduce(CharSequence key, Iterable<Integer> values, AvroCollector<Pair<CharSequence, Integer>> collector, Reporter reporter)
+        public void reduce(Integer key, Iterable<CharSequence> values, AvroCollector<Pair<Integer, CharSequence>> collector, Reporter reporter)
                 throws IOException {
-            int sum = 0;
-            for (Integer value: values){
-                sum += value;
+            String genre = "";
+            for (CharSequence value: values){
+                genre += value.toString() + ", ";
             }
-            collector.collect(new Pair<>(key, sum));
+            collector.collect(new Pair<>(key, genre));
         }
     }
 
     public int run(String[] args) throws Exception {
         if(args.length != 2){
-            System.err.println("Usage: PopularGenresByYear <input path> <output path>");
+            System.err.println("Usage: SortPopularGenresByYear <input path> <output path>");
             return -1;
         }
-        JobConf conf = new JobConf(getConf(), PopularGenresByYear.class);
-        conf.setJobName("PopularGenresByYearMapRed");
+        JobConf conf = new JobConf(getConf(), SortGenresByYear.class);
+        conf.setJobName("SortPopularGenresByYearMapRed");
 
         Path outputPath = new Path(args[1]);
         outputPath.getFileSystem(conf).delete(outputPath, true);
@@ -68,18 +66,18 @@ public class PopularGenresByYear extends Configured implements Tool {
         FileInputFormat.setInputPaths(conf, new Path(args[0]));
         FileOutputFormat.setOutputPath(conf, new Path(args[1]));
 
-        AvroJob.setMapperClass(conf, PopularGenresMapper.class);
-        AvroJob.setReducerClass(conf, PopularGenresReducer.class);
+        AvroJob.setMapperClass(conf, SortPopularGenresMapper.class);
+        AvroJob.setReducerClass(conf, SortPopularGenresReducer.class);
 
-        AvroJob.setInputSchema(conf, genreYear);
-        AvroJob.setOutputSchema(conf,Pair.getPairSchema(Schema.create(Type.STRING),Schema.create(Type.INT)));
+        AvroJob.setInputSchema(conf, genreYearCount);
+        AvroJob.setOutputSchema(conf,Pair.getPairSchema(Schema.create(Type.INT),Schema.create(Type.STRING)));
 
         JobClient.runJob(conf);
         return 0;
     }
 
     public static void main(String[] args) throws Exception{
-        int res = ToolRunner.run(new Configuration(), new PopularGenresByYear(), args);
+        int res = ToolRunner.run(new Configuration(), new SortGenresByYear(), args);
         Configuration conf = new Configuration();
         FileSystem fs = FileSystem.get(conf);
     
@@ -89,7 +87,7 @@ public class PopularGenresByYear extends Configured implements Tool {
             for (File outputFile : outputFiles) {
                 if (outputFile.getName().endsWith(".avro")) {
                 String textName = outputFile.getName().replace(".avro", ".txt");
-                List<String> records = DeserializationData.getRecords(outputFile.getAbsolutePath(), "string", "int");
+                List<String> records = DeserializationData.getRecords(outputFile.getAbsolutePath(), "int", "string");
                 File textFile = new File(outputFile.getParent(), textName);
                 FileUtils.writeLines(textFile, records);
                 }
@@ -100,5 +98,4 @@ public class PopularGenresByYear extends Configured implements Tool {
         }
         System.exit(res);
     }
-    
 }

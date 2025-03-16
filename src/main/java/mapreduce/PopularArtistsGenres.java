24c76c4f9131ad56335c1dd0ec770d04f741fdfd
AndreaDiMartin <18-10826@usb.ve>
2025-03-16 18:14:09 -0400

    Refactor PopularArtistsGenres for improved clarity and functionality, including enhanced comments, genre handling, and input validation

--- src/main/java/mapreduce/PopularArtistsGenres.java
+++ src/main/java/mapreduce/PopularArtistsGenres.java
@@ -1,6 +1,6 @@
 package mapreduce;
 
-//Artistas con popularidad mayor a 60 y sus generos
+//Problema 3: Géneros de los artistas más populares
 
 import java.io.IOException;
 import java.io.OutputStreamWriter;
@@ -25,29 +25,29 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class PopularArtistsGenres extends Configured implements Tool {
+    //Géneros principales más populares
     private static final List<String> GENRES = Arrays.asList(
-            "hop", "country", "rock", "jazz", "pop", "reggae", "metal", "blues", "rap", "blues", "classical", "house", "folk", "dance",
+            "hop", "country", "rock", "jazz", "pop", "reggae", "metal", "rap", "blues", "classical", "house", "folk", "dance",
             "r&b", "indie", "punk", "electronic", "hardcore", "trap"
         );
 
     private static Object[] CSVParser(String line){
         List<String> spotifyLine = new ArrayList<>();
         StringBuilder currentField = new StringBuilder();
-        // Create a flag to keep track of whether we're inside quotes
+        // Crea un flag para tomar en cuenta si se está dentro de comillas
         boolean inQuotes = false;
 
-        // Iterate over the characters in the line
+        // Itera por los caracteres de una linea 
         for (char c : line.toCharArray()) {
-            // Check if the character is a quote
+            // Verifica si el caracter es una comilla
             if (c == '"') {
-                // If it is, toggle the inQuotes flag
-                inQuotes = !inQuotes; // Toggle the inQuotes flag
+                inQuotes = !inQuotes; // Cambia el estado de inQuotes
             } else if (c == ',' && !inQuotes) {
-                // If we encounter a comma and we're not inside quotes, it's the end of a field
+                // Si el caracter es una coma y no está dentro de comillas, agrega el campo actual a la lista
                 spotifyLine.add(currentField.toString());
-                currentField.setLength(0); // Clear the current field
+                currentField.setLength(0); // Se limpia el campo actual
             } else {
-                // Otherwise, just add the character to the current field
+                // Si no es una coma o una comilla, se agrega el caracter al campo actual
                 currentField.append(c);
             }
         }
@@ -63,19 +63,21 @@ public class PopularArtistsGenres extends Configured implements Tool {
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
             String line = value.toString();
             Object[] lineArray = CSVParser(line);
+            // Ignora la primera línea del archivo (header)
             if(!lineArray[0].equals("id")){
                 Integer artistPopularity = Integer.parseInt(lineArray[28].toString());
-                if(artistPopularity > 70){
+                if(artistPopularity > 70){ //Seleccionamos los artistas con un popularidad mayor a 70
                 String genre_id = lineArray[30].toString();
                 String[] genreSplit = genre_id.toString().split(" ");
                 String mainGenre = genreSplit[genreSplit.length - 1];
+                //Verificamos si es posible agrupar el subgenero
                 if (GENRES.contains(mainGenre)) {
                     genre_id = mainGenre;
                 }
+                //Si el género está vacío entonces el género se pasa a Unknown
                 if(genre_id.equals("")){
-                    genre_id = "Unknown";
+                    genre_id = "unknown";
                 }
-                //System.out.println(lineArray[28] +  "," + lineArray[30]);
                 context.write(new Text("{" + genre_id + "}"), new IntWritable(1));
                 }
             }
@@ -86,6 +88,7 @@ public class PopularArtistsGenres extends Configured implements Tool {
     public static class PopularArtistsGenresReducer extends Reducer<Text, IntWritable, Text, IntWritable>{
         @Override
         protected void reduce(Text key,Iterable<IntWritable> values, Context context) throws IOException, InterruptedException{
+            //Se suman los valores para obtener la cuenta de cuántos artistas pertenecen a cierto género
             Integer sum = 0;
             for(IntWritable value: values){
                 sum += value.get();
@@ -95,14 +98,22 @@ public class PopularArtistsGenres extends Configured implements Tool {
     }
 
     public int run(String[] args) throws Exception {
+        //Se verifica la longitud de los argumentos
         if(args.length != 2){
             System.err.println("Usage: PopularArtistsGenre <input path> <output path>");
             System.exit(-1);
         }
+        //Se obtiene la configuración de hadoop
         Configuration conf = getConf();
         
+        //Se le da nombre al trabajo
         Job job = Job.getInstance(conf, "PopularArstistsGenre");
 
+        //Se borran la carpeta del path de salida si ya existe
+        Path outputPath = new Path(args[1]);
+        outputPath.getFileSystem(conf).delete(outputPath, true);
+
+        //Se designan los paths de los archivos de entrada y salida
         FileInputFormat.setInputPaths(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 

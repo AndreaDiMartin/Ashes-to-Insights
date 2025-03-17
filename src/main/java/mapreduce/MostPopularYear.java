2e2f8aaf8656b2f0ec0695149e22b144be14984c
Daniela <19-10105@usb.ve>
2025-03-16 21:00:57 -0400

    Add Years with the most populary songs

--- src/main/java/mapreduce/MostPopularYear.java
+++ src/main/java/mapreduce/MostPopularYear.java
@@ -23,69 +23,70 @@ import org.apache.hadoop.mapred.Reporter;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
-import classes.avro.spotify;
+import classes.avro.spotify; 
 
+public class MostPopularYear extends Configured implements Tool { 
+    public static class PopularityMapper extends AvroMapper<spotify, Pair<Integer, Integer>> {
 
-public class LongestTrackByYear extends Configured implements Tool {
-
-    public static class TrackMapper extends AvroMapper<spotify, Pair<Integer, Integer>> {
-        
         @Override
         public void map(spotify spotifyRecord, AvroCollector<Pair<Integer, Integer>> collector, org.apache.hadoop.mapred.Reporter reporter) throws IOException {
-            Integer yearOfRelease = spotifyRecord.getYearOfRelease();
-            CharSequence trackNameUtf8 = spotifyRecord.getTrackName();
+            Integer year =  spotifyRecord.getYearOfRelease();
+            Integer popularity = (Integer) spotifyRecord.getPopularity();
+            
 
-            if (yearOfRelease == null) {
-                yearOfRelease = 0;
+            if (year == null || year == 1) {
+                year = 0;
             }
 
-            String trackName = trackNameUtf8 != null ? trackNameUtf8.toString() : "";
-            trackName = trackName.trim().replaceAll("[^a-zA-Z ]", "").toLowerCase();
-            
-            collector.collect(new Pair<>(yearOfRelease, trackName.length()));
+            if (popularity >= 50) {
+                collector.collect(new Pair<>(year, 1));
+            }
         }
     }
 
-    public static class LongestTrackReducer extends AvroReducer<Integer, Integer, Pair<Integer, Integer>> {
+
+    public static class PopularityReducer extends AvroReducer<Integer, Integer, Pair<Integer, Integer>> {
+        
         @Override
         public void reduce(Integer key, Iterable<Integer> values, AvroCollector<Pair<Integer, Integer>> collector, Reporter reporter) throws IOException {
-            int maxLength = 0;
-            for (Integer length : values) {
-                maxLength = Math.max(maxLength, length);
+            int count = 0;
+            for (Integer value : values) {
+                count += value;
             }
-            collector.collect(new Pair<>(key, maxLength));
+            collector.collect(new Pair<>(key, count));
         }
     }
 
+
     public int run(String[] args) throws Exception {
-        if (args.length != 2) {
-            System.err.println("Usage: LongestTrackByYear <input path> <output path>");
+        if(args.length != 2){
+            System.err.println("Usage: MostPopularYear <input path> <output path>");
             return -1;
         }
 
-        JobConf conf = new JobConf(getConf(), LongestTrackByYear.class);
-        conf.setJobName("LongestTrackByYear");
+        JobConf conf = new JobConf(getConf(), MostPopularYear.class);
+        conf.setJobName("MostPopularYearMapRed");
 
         Path outputPath = new Path(args[1]);
         outputPath.getFileSystem(conf).delete(outputPath, true);
-    
-        FileInputFormat.setInputPaths(conf, new Path(args[0]));
+
+        FileInputFormat.addInputPath(conf, new Path(args[0]));
         FileOutputFormat.setOutputPath(conf, new Path(args[1]));
-    
-        AvroJob.setMapperClass(conf, TrackMapper.class);
-        AvroJob.setReducerClass(conf, LongestTrackReducer.class);
+
+        AvroJob.setMapperClass(conf, PopularityMapper.class);
+        AvroJob.setReducerClass(conf, PopularityReducer.class);
 
         AvroJob.setInputSchema(conf, spotify.getClassSchema());
-        AvroJob.setOutputSchema(conf, Pair.getPairSchema(Schema.create(Type.INT), Schema.create(Type.INT)));
-    
+        AvroJob.setOutputSchema(conf ,Pair.getPairSchema(Schema.create(Type.INT),Schema.create(Type.INT)));
+
         JobClient.runJob(conf);
         return 0;
     }
 
-    public static void main(String[] args) throws Exception {
-        int res = ToolRunner.run(new Configuration(), new LongestTrackByYear(), args);
-
-        if (res == 0) {
+    public static void main(String[] args) throws Exception{
+        int res = ToolRunner.run(new Configuration(), new MostPopularYear(), args);
+    
+        if(res == 0){
             File outputDir = new File(args[1]);
             File[] outputFiles = outputDir.listFiles();
             for (File outputFile : outputFiles) {
@@ -96,12 +97,11 @@ public class LongestTrackByYear extends Configured implements Tool {
                 FileUtils.writeLines(textFile, records);
                 }
             }
-            System.out.println("Job executed successfully");
+            System.out.println("Trabajo terminado con exito");
         } else {
-            System.out.println("Job failed");
+            System.out.println("Trabajo falló");
         }
-
+        
         System.exit(res);
     }
 }
-
